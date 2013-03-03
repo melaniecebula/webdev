fe29d593ab82bc2b9cd1d90352848f6e68a1e8e9
Melanie Cebula <melaniecebula@berkeley.edu>
2013-03-02 17:54:31 -0800

    initiating

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Webdevrails::Application.config.secret_key_base = 'aca7af2b9d0ae5e16652174df31166d9e7a53b83f9454a36bcf8a105e9f3b2c8805b005548d77c19ce93105fa09e0650554bb5b5c466b8c970f369879ed23d40'
